513bfdd82f14c6ca2c6b701f312a2dfc2ec8637d
yassineboucham <yassineboucham2002@gmail.com>
2024-11-28 13:51:28 +0100

    0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python3
+'''Task 0: Basic Flask app
+'''
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    '''default route'''
+    return render_template("0-index.html",)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True)
